7c692c988e4257eb351d1bb202217bd3696960e4
Loic NOEL <loicnoel12@gmail.com>
2023-02-21 08:12:52 +0100

    Initial commit

---
+++ src/main/kotlin/com/lno/MakeMyDinner/MakeMyDinnerApplication.kt
@@ -0,0 +1,11 @@
+package com.lno.MakeMyDinner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MakeMyDinnerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MakeMyDinnerApplication>(*args)
+}
---
+++ src/test/kotlin/com/lno/MakeMyDinner/MakeMyDinnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lno.MakeMyDinner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MakeMyDinnerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
